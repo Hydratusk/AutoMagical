293f8eb4fd80c80d83a216b9d8cfa14530293869
Hydratusk <michael.a.sanchez@asu.edu>
2012-04-14 18:42:07 -0700

    First Project upload

---
+++ AM/src/HelloGit.java
@@ -0,0 +1,13 @@
+
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello there world I am from accross the wire.");
+
+	}
+
+}
