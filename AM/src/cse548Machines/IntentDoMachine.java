1d4395676d9851f9699b1229c9dd2c93ec79f868
Hydratusk <michael.a.sanchez@asu.edu>
2012-04-16 14:31:44 -0700

    Changed Location of IntentDo to cse548 specific package

---
+++ AM/src/cse548Machines/IntentDoMachine.java
@@ -0,0 +1,61 @@
+package cse548Machines;
+
+import java.util.ArrayList;
+
+import FiniteStateMachine.FSAM;
+
+import cse548interfaces.Wave;
+
+public class IntentDoMachine extends FSAM implements cse548interfaces.deltable{
+
+	
+	public IntentDoMachine(String ServentType) {
+		super(ServentType+"IntentDo", ServentType);
+		
+	}
+
+	@Override
+	public String getVariablesFromWave(Wave incomingWave) {
+		return incomingWave.getIntentDo();
+	}
+
+	@Override
+	public void setWaveVariables(Wave incomingWave) {
+		incomingWave.setIntentDo(super.getFieldValue("WaveMSG"));
+		
+	}
+
+	@Override
+	public boolean delta(Wave incomingWave) {
+		return super.delta(getVariablesFromWave(incomingWave));
+	}
+
+	@Override
+	public boolean delta(boolean okay) {
+		String interim = okay?"true":"false";
+		return super.delta(interim);
+		
+	}
+	
+	@Override
+	public String getMessagesToUser()
+	{
+		//ArrayList<String> ret = new ArrayList<String>();
+		return super.getMessagesToUser();
+		//return ret;
+	}
+
+	@Override
+	public String NeededInput()
+	{
+		
+		return super.NeededInput();
+		
+	}
+
+	@Override
+	public boolean isFinished()
+	{
+		return super.isFinished();
+	}
+}
